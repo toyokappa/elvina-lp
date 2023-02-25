d4515856bd9c1427a7b243d3db3e9debb1546154
Yuta Toyokawa <kppg42@gmail.com>
2023-02-26 01:25:57 +0900

    規約系ページの追加

---
+++ pages/tokusho.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+section.section
+  .px-2
+    section.mb-2
+      h1.mb-2 特定商取引法に基づく表示
+    section.mb-2
+      h2.mb-1 1. 事業者の名称
+      p.mb-2 パーソナルジムElvina
+    section.mb-2
+      h2.mb-1 2. 所在地
+      p.mb-2 〒371-0842 群馬県前橋市下石倉町12-4
+    section.mb-2
+      h2.mb-1 3. 電話番号
+      p.mb-2 080-7004-8979
+    section.mb-2
+      h2.mb-1 4. 営業時間
+      p.mb-2 10:00〜22:00
+    section.mb-2
+      h2.mb-1 5. 利用料金
+      p.mb-2 ご利用料金は、サービスごとに表示された金額と致します。
+    section.mb-2
+      h2.mb-1 6. 利用条件
+      p.mb-2
+        | ご利用いただくサービスのご利用条件は、
+        nuxt-link(to="/terms") 利用規約
+        | をご覧ください。
+    section.mb-2
+      h2.mb-1 支払い方法
+      p.mb-2 現金・クレジットカード（VISA / MasterCard / JCB / American Express / Diners Club / Discover）でのお支払いが可能です。
+    section.mb-2
+      h2.mb-1 支払い時期
+      p.mb-2 クレジットカードの決済は、サービス利用の予約時。お支払日は、ご利用のクレジットカードの規定によります。
+    section.mb-2
+      h2.mb-1 変更・キャンセル手続き対応
+      p.mb-2
+        | 変更・キャンセル手続については、
+        nuxt-link(to="/terms") 利用規約
+        | をご覧ください。
+    section.mb-2
+      h2.mb-1 個人情報の取扱いについて
+      p.mb-2
+        | 個人情報の取扱いに関しては、
+        nuxt-link(to="/policy") プライバシーポリシー
+        | をご参照ください。
+</template>
+
+<style lang="sass" scoped>
+.section
+  font-size: 14px
+</style>
