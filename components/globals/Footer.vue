85c05ade07cf2bd74cff8fbaa7fcd44571eeea88
Yuta Toyokawa <kppg42@gmail.com>
2023-03-25 06:51:19 +0900

    利用規約などの追加

--- components/globals/Footer.vue
+++ components/globals/Footer.vue
@@ -1,5 +1,13 @@
 <template lang="pug">
-footer &copy; ELVINA 2023
+footer
+  ul.footer-nav
+    li
+      nuxt-link(to="/terms") 利用規約
+    li
+      nuxt-link(to="/policy") プライバシーポリシー
+    li
+      nuxt-link(to="/tokusho") 特定商取引法に基づく表示
+  .copy &copy; ELVINA 2023
 </template>
 
 <style lang="sass" scoped>
@@ -11,4 +19,14 @@ footer
   background-color: $dark
   width: 100%
   padding: $block-padding
+  .footer-nav
+    list-style: none
+    text-align: center
+    margin: 0
+    padding: 10px 0
+    li
+      margin-bottom: 5px
+      a
+        color: inherit
+        text-decoration: none
 </style>
