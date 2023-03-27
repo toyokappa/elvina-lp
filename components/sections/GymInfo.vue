ad1ca843f99df6d510fcb3f3763c37e10c00d96b
Yuta Toyokawa <kppg42@gmail.com>
2023-03-27 20:24:17 +0900

    地図情報を変更

--- components/sections/GymInfo.vue
+++ components/sections/GymInfo.vue
@@ -10,7 +10,7 @@ section.section(:class="{ 'bg-base': bgBase }")
       p 群馬県前橋市下石倉町12-4
       p AZBL BOXを利用
   .map.eyecatch
-    iframe(src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4564.642914470674!2d139.05398959383797!3d36.38047622690952!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa8a4e5446f4dc5ed!2z44OR44O844K944OK44Or44OI44Os44O844OL44Oz44Kw44K444OgQVpCTCBQUklWQVRF5YmN5qmL5LiL55-z5YCJ5bqXIC8g44Os44Oz44K_44Or44K444OgQVpCTCBCT1jliY3mqYvkuIvnn7PlgInlupc!5e0!3m2!1sja!2sjp!4v1670689512120!5m2!1sja!2sjp" title="gym map" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade")
+    iframe(src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12848.69494225413!2d139.0573082!3d36.380788!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ef317f0557217%3A0x69ce32a6d429180c!2z5aWz5oCn5bCC55So44OR44O844K944OK44Or44K444OgRWx2aW5h!5e0!3m2!1sja!2sjp!4v1679916177725!5m2!1sja!2sjp" title="gym map" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade")
 </template>
 
 <script setup lang="ts">
